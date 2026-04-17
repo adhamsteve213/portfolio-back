68418555bc8c96c85d9c75b61172b55c90238d76
adhamsteve213 <adhamsteve21@gmail.com>
2026-04-17 22:46:04 +0200

    admin auth done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::query()->updateOrCreate([
-            'email' => env('ADMIN_EMAIL', 'admin@portfolio.test'),
+            'email' => env('ADMIN_EMAIL', 'adhamsteve213@gmail.com'),
         ], [
             'name' => env('ADMIN_NAME', 'Portfolio Admin'),
-            'password' => Hash::make(env('ADMIN_PASSWORD', 'admin12345')),
+            'password' => Hash::make(env('ADMIN_PASSWORD', 'mms183121')),
             'is_admin' => true,
         ]);
     }
